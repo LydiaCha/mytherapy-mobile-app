531ec21124e1951eb84181c1db1a12f04d0711fd
Lydia <lydia.chatziioannou@gmail.com>
2020-08-09 13:28:37 +0100

    pulls data from mysql db

---
+++ App.js
@@ -0,0 +1,62 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, Component } from 'react';
+import { StyleSheet, Text, View, Button, RefreshControl, TextInput, FlatList, SectionList, ActivityIndicator } from 'react-native';
+import { RecyclerListView } from "recyclerlistview";
+
+export default class App extends Component {
+constructor() {
+  super()
+  this.state = {
+    dataSource: []
+  }
+}
+
+renderItem = ({ item }) => {
+  return (
+    <View>
+    <View>
+      <Text>
+        {item.title}
+      </Text>
+    </View>
+    <View>
+      <Text>
+        {item.content}
+      </Text>
+    </View>
+    </View>
+  )
+}
+
+componentDidMount() {
+  const url = 'http://localhost:4000/articles'
+  fetch(url)
+  .then((response) => response.json())
+  .then((responseJson) => {
+this.setState({
+  dataSource: responseJson.data
+})
+  })
+}
+
+  render() {
+return(
+    <View style={styles.container}>
+      <FlatList
+        data={this.state.dataSource}
+        renderItem={this.renderItem}
+      />
+    </View>
+);
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+  
+});
+
